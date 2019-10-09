9664e2701886e4331f2f016eebc35e71241918df
Konstantin Igorevich <>
2019-10-10 00:40:23 +0300

    create news interface

---
+++ developer.nytimes/developer.nytimes/Sources/MainTabBarScreen/NewsCell/NewsCell.swift
@@ -0,0 +1,34 @@
+//
+//  NewsCell.swift
+//  developer.nytimes
+//
+//  Created by Konstantin Igorevich on 09.10.2019.
+//  Copyright © 2019 Konstantin Igorevich. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UICollectionViewCell {
+    
+    @IBOutlet weak private var imgNews: UIImageView!
+    @IBOutlet weak private var lbNewsTitle: UILabel!
+    @IBOutlet weak private var lbNewsDescription: UILabel!
+    @IBOutlet weak private var lbNewsDay: UILabel!
+    @IBOutlet weak private var lbNewsMonth: UILabel!
+    @IBOutlet weak private var btFavourites: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configuredCellWith() {
+        
+    }
+    
+    @IBAction func didTabAddNewsToFavouritesButton(_ sender: IBAction) {
+        // TODO - Add news to favourites
+        print("didTabAddNewsToFavouritesButton")
+    }
+
+}
